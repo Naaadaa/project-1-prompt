ef6a3077852df32d5af3d0851c34b76dbb1dc317
nadaalotaibii <naaadaa_tb@yahoo.com>
2019-07-04 08:14:09 +0300

    project

--- app.js
+++ app.js
@@ -2,53 +2,55 @@
 
 
 	var turn = 0; // player 
-	
+
 	$(".p").text("Let's play the game !");
-	
+
 	$(".cell").one("click",function () {
 		// for each click append x or o . 
 			if ((turn % 2) == 0) { 
-				
+
 				$(this).append("X");
-	           
-				$(this).css('color', "palevioletred");
-				$(".p").html("<span class='p'> Player O's turn! </span>");
-			
-		
+
+				$(this).css({'color':"palevioletred" ,
+			                 "font-family":"Boogaloo"});
+				$(".p").html("<span class='p'>  O's turn! </span>");
+          
+
 				}
 
-				
+
 		 else {
 				$(this).append("O");
-				$(this).css('color', "palevioletred");
-				$(".p").html("<span class='p'> Player X's turn! </span>");
+				$(this).css({'color': "palevioletred",
+				        "font-family":"Boogaloo"});
+				$(".p").html("<span class='p'>  X's turn! </span>");
 
 			}
 			turn++;
 			 // check the winner 
-	
+
 			if (checkWinner() != false ) {
 				if (checkWinner() == "X") {
 					$(".p").text(" Player X Wins !");
 					 $(".cell").off("click"); 
 				} else if (checkWinner() == "O") {
-					
-					
+
+
 					$(".p").text(" Player O Wins !"); 
 					$(".cell").off("click"); 
-					
-					
+
+
 				}
-				
+
 			}
 			else if (turn ==9) {
 				$(".p").text(" No one wins the game !"); 
 			 }
-		
+
 	});
 
 	let checkWinner = function () {
-		
+
 		var cell1 = $("#c1").text();
         var cell2 = $("#c2").text();
         var cell3 = $("#c3").text();
@@ -59,7 +61,7 @@
         var cell8 = $("#c8").text();
         var cell9 = $("#c9").text();
 
-	
+
 
 
 		// check rows
@@ -88,8 +90,4 @@
 			return false;
 		 }
 	}
-
-
-
-
 
